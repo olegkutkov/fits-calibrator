87bae766451c9662bf4963981288f7b120eced2c
Oleg Kutkov <elenbert@gmail.com>
2017-11-16 02:27:16 +0300

    build master dark from multiple darks

--- src/calibrator.c
+++ src/calibrator.c
@@ -76,16 +76,14 @@ int substract_darks(calibrator_params_t *params, const char *src_file, time_t im
 	char *full_file_path = NULL;
 	double dark_exposure, exp_diff, min_exp, max_exp;
 	time_t dark_date, timediff_sec, min_time, max_time;
-	fits_handle_t **dark_images = NULL;
-	fits_handle_t *curr_dark;
+	fits_handle_t *curr_dark = NULL;
+	fits_handle_t *master_dark = NULL;
 
 	dp = opendir(params->darkpath);
 
 	if (dp == NULL) {
 		return -1;
 	}
-
-	dark_images = (fits_handle_t **) malloc (sizeof(fits_handle_t *) * params->max_calfiles + 1);
 
 	while ((ep = readdir(dp))) {
 		if (dark_counter >= params->max_calfiles) {
@@ -123,15 +121,30 @@ int substract_darks(calibrator_params_t *params, const char *src_file, time_t im
 
 				if (exp_diff >= params->min_exp_eq_percent) {
 					params->logger_msg("\t!!! Found corresponding dark %s to file %s, timediff: %li sec, expdiff %.2f %%\n",
-										full_file_path, src_file, timediff_sec, exp_diff);
+											full_file_path, src_file, timediff_sec, exp_diff);
 
-					dark_images[dark_counter] = curr_dark;
+					fits_load_image(curr_dark);
+
+					if (dark_counter == 0) {
+						status = 0;
+
+						master_dark = fits_handler_mem_new(&status);
+
+						fits_create_image_mem(master_dark, fits_get_image_w(curr_dark), fits_get_image_h(curr_dark));
+
+						fits_copy_image(master_dark, curr_dark);
+					} else {
+						fits_add_image_matrix(master_dark, curr_dark);
+					}
+
+					fits_free_image(curr_dark);
+
 
 					dark_counter++;
 				}
-			} else {
-				fits_handler_free(curr_dark);
 			}
+
+			fits_handler_free(curr_dark);
 
 			free(full_file_path);
 		}
@@ -140,11 +153,18 @@ int substract_darks(calibrator_params_t *params, const char *src_file, time_t im
 	closedir (dp);
 
 	if (dark_counter < params->min_calfiles) {
-		free_darks_list(dark_images, dark_counter);
+		if (master_dark) {
+			free(master_dark);
+		}
+
 		return -1;
 	}
 
-	free_darks_list(dark_images, dark_counter);
+	fits_divide_image_matrix(master_dark, dark_counter);
+
+	if (master_dark) {
+		free(master_dark);
+	}
 
 	return 0;
 }
@@ -179,10 +199,20 @@ void calibrate_one_file(const char *file, void *arg)
 
 	if (strlen(params->darkpath) > 0) {
 		substract_darks(params, file, image_time, image_exptime);
-		//calib_image = find_best_calibration_file(params, image_time, object);
 	}
 
 	fits_handler_free(fits_image);
+
+
+	task_enter_critical_section();
+
+	total_files_counter--;
+
+	if (total_files_counter == 0) {
+		params->complete();
+	}
+
+	task_exit_critical_section();
 }
 
 void *thread_func(void *arg)
